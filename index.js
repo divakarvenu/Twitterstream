45105ebc7d408bee54cb561969d17870b048444c
Nithyanandam <divakarvenu26@gmail.com>
2019-01-06 18:06:18 +0530

    server code updated and oauth tokens removed

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
-var Twit = require('twit')
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
+var Twit = require('twit');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+var io = require('socket.io')(server);
 var fs = require('fs');
 
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+
 
 io.on('connection', function(socket) {
+
+    T.get('search/tweets', { q: '#fun', count: 100 }, function(err, data, response) {
+      var tweetArray=[];
+        for (let index = 0; index < data.statuses.length; index++) {
+            const tweet = data.statuses[index];
+            var tweetbody = {
+              'text': tweet.text,
+              'userScreenName': "@" + tweet.user.screen_name,
+              'userImage': tweet.user.profile_image_url_https,
+              'userDescription': tweet.user.description,
+            }
+            try {
+              if(tweet.entities.media[0].media_url_https) {
+                tweetbody['image'] = tweet.entities.media[0].media_url_https;
+              }
+            } catch(err) { }
+            tweetArray.push(tweetbody);
+        }     
+        io.emit('allTweet',tweetArray)
+    })
+
     var stream = T.stream('statuses/filter', { track: '#fun', language: 'en' })
 
     stream.on('tweet', function (tweet) {
         io.emit('tweet',{ 'tweet': tweet });
     })
 });
+  var T = new Twit({
+      consumer_key:         '',
+      consumer_secret:      '',
+      access_token:         '',
+      access_token_secret:  '',
+      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  });
 
-var T = new Twit({
-    consumer_key:         '1vlF5wme6BuyZgANV1LGffFrG',
-    consumer_secret:      'd3v4fx1OoD5eN5vLujtED7MIySLyc46ciMRx8rDrQ3m1cUpY7t',
-    access_token:         '1333236546-EoofSCeyW9hMfxHAcsrit6bbZcPq4NLAkGI1TOX',
-    access_token_secret:  'zOs7RtUAShsgzbdRHknl5eYSGlnJs7xh6sueGS1C7B8xW',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
-
-
-
-
-app.listen(8080);
-
+// listen for requests :)
+const listener = server.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
