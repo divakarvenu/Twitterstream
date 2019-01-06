ebdd500fcc038d22c80d05811bd5dd26bebf152e
Nithyanandam <divakarvenu26@gmail.com>
2019-01-06 18:18:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + '/index.html');
 });
 
 
